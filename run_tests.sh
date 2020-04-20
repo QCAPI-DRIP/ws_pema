d5731450ec594b9d441b5183fc1e20ff6cb0d965
skoulouzis <skoulouzis@users.noreply.github.com>
2020-04-20 16:54:08 +0200

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -6,3 +6,11 @@ host=localhost:8080
 curl -v -X PUT http://$host/$yourprojectname
 
 filename=test_dataset/parameters.tsv
+inputtemplate=metadata
+curl -v -F "inputtemplate=$inputtemplate" -F "file=@test_dataset/$filename" http://$host/$yourprojectname/input/$filename
+
+
+filename=ERR1906859_1.fastq.gz
+inputtemplate=mydata
+curl -v -F "inputtemplate=$inputtemplate" -F "file=@test_dataset/$filename" http://$host/$yourprojectname/input/$filename
+
